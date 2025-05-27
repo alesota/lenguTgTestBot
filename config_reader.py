f2c5517a07bddd845c93d194d89ad8e2866186a7
alesha shit coder/ <168659571+alesota@users.noreply.github.com>
2025-05-28 01:00:00 +0300

    Add files via upload

---
+++ config_reader.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from pydantic import SecretStr, Field
+from typing import List
+
+class Settings(BaseSettings):
+    BOT_TOKEN: SecretStr
+    ADMINS: str  
+    GROUP_ID: str
+    
+    @property
+    def admin_list(self) -> list[int]:
+        return [int(x.strip()) for x in self.ADMINS.split(',') if x.strip()]
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+        extra = 'ignore'
+
+config = Settings()
